3e44594fa8ecac5d11c61cc7523352c62d710066
funfacttriviawithme-ctrl <funfacttriviawithme@gmail.com>
2025-12-30 16:15:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // === KONEKSI MONGODB ===
-const MONGODB_URI = process.env.MONGODB_URL || 'mongodb+srv://inklusiteknologi:EhJayo84M9IxUTd@inklusiteknologi.g9awxur.mongodb.net/inklusiteknologi?retryWrites=true&w=majority';
+const MONGODB_URI = 'mongodb://localhost:27017/inklusiteknologi';
 mongoose.connect(MONGODB_URI);
 
 // === MODEL ===
